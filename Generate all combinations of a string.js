5b4b7f45a800ad73d2774ce989eeaa24bf24612d
Utkarsh Rosario <93281920+utkarshrosario@users.noreply.github.com>
2023-01-27 16:36:33 +0530

    Update Generate all combinations of a string.js

--- Generate all combinations of a string.js
+++ Generate all combinations of a string.js
@@ -1,7 +1,7 @@
 // Generating all combinations of a string
 function combination_of_string (str)
 {
-    let string = new Array();
+    var string = new Array();
     for(var x = 0; x <= str.length; x++)
     {
         for(var y = x + 1; y <= str.length; y++)
