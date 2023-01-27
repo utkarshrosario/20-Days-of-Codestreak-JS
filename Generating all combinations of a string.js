80cfa0514abbb27ed679e2e6d0998f82c2dde41f
Utkarsh Rosario <93281920+utkarshrosario@users.noreply.github.com>
2023-01-27 16:38:30 +0530

    Create Generating all combinations of a string.js

---
+++ Generating all combinations of a string.js
@@ -0,0 +1,15 @@
+// Generating all combinations of a string
+function combination_of_string (str)
+{
+    var string = new Array();
+    for(var x = 0; x <= str.length; x++)
+    {
+        for(var y = x + 1; y <= str.length; y++)
+        {
+            string.push(str.slice(x, y));
+        }
+    }
+    return string;
+}
+// Input
+document.write(combination_of_string("codestreak"));
